06886836964c71490c2dbda5c961ce2c0b313b53
Thiago Precivalli <stuart_fsi05@hotmail.com>
2026-01-05 07:05:32 -0300

    Fix: First Principles update - Android SDK 35, Process.env, Lint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,31 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  globalIgnores(['dist', 'android']),
+  { extends: [js.configs.recommended, ...tseslint.configs.recommended] },
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^[A-Z_]' }],
+      '@typescript-eslint/no-explicit-any': 'warn'
     },
   },
-])
+)
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  define: {
+    'process.env': {}
+  },
   plugins: [
     react(),
     VitePWA({
